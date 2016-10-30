524a7a5052963a112a01505eeb731b9adaecc7bd
Pat O'Connor <patoconnor43@gmail.com>
2016-10-30 11:20:02 -0700

    Add TestBackupCopy

--- linker_test.go
+++ linker_test.go
@@ -19,6 +19,7 @@ func TestNeedSymlink(t *testing.T) {
 	// Test dest does not exist
 	src := "/tmp/dotbro/linker/TestNeedSymlink.txt"
 	dest := "/tmp/dotbro/linker/TestNeedSymlink.txt"
+	wrongDest := "/tmp/dotbro/linker/wrongTestNeedSymlink"
 
 	actual, err := needSymlink(src, dest)
 	assert.True(t, true)
@@ -48,6 +49,19 @@ func TestNeedSymlink(t *testing.T) {
 		t.Fatal(err)
 	}
 	assert.False(t, actual)
+
+	// Test symlink goes to the wrong destination
+	if err = os.Remove(dest); err != nil {
+		t.Fatal(err)
+	}
+	if err = os.Symlink(wrongDest, dest); err != nil {
+		t.Fatal(err)
+	}
+	actual, err = needSymlink(src, dest)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.True(t, actual)
 
 }
 
@@ -100,4 +114,21 @@ func TestBackup(t *testing.T) {
 	}
 	err = backup(dest, destAbs, backupDir)
 	assert.Empty(t, err)
+}
+
+func TestBackupCopy(t *testing.T) {
+	filename := "/tmp/dotbro/linker/TestBackupCopy/file"
+	backupDir := "/tmp/dotbro/linker/TestBackupCopy/backup"
+	if err := os.MkdirAll(path.Dir(filename), 0755); err != nil {
+		t.Fatal(err)
+	}
+	if err := ioutil.WriteFile(filename, []byte("TestData"), 0755); err != nil {
+		t.Fatal(err)
+	}
+
+	err := backupCopy(filename, backupDir)
+	assert.Empty(t, err)
+	filenameContent, _ := ioutil.ReadFile(filename)
+	backupContent, _ := ioutil.ReadFile(backupDir + "/file")
+	assert.Equal(t, filenameContent, backupContent)
 }
